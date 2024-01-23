b4dac9c46103fe148fdde60a42e73da9ffea05d8
Pavan Dhadge <dhadgepavan00@gmail.com>
2024-01-23 22:05:28 +0530

    utils codes

--- src/index.js
+++ src/index.js
@@ -7,3 +7,16 @@ dotenv.config({
 })
 
 dbConnect()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("error :",error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("server is running at port = "+`${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+        console.log("MONGOO DB connection failed !!!!",err)
+
+})
---
+++ src/utils/apierror.js
@@ -0,0 +1,24 @@
+const errmassage = "something went wrong"
+
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message=`${errmassage}`,
+        errors=[],
+        stack =""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = nullthis.message = message
+        this.success = false
+        this.errors = errors
+
+        if(stack){
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {ApiError}
